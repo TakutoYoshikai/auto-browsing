6184c9996656aeaccd8ffca99364cb4dc82e7c87
Takuto Yoshikai <takuto.yoshikai@gmail.com>
2019-05-27 13:49:54 +0900

    a要素が見つからなかった時の処理実装

--- main.rb
+++ main.rb
@@ -4,7 +4,11 @@ driver.navigate.to "https://news.yahoo.co.jp/"
 
 while true do 
   a_list = driver.find_elements(:tag_name, "a")
+  if a_list.length == 0
+    driver.navigate.to "https://news.yahoo.co.jp/"
+    next
+  end
   a = a_list[rand(a_list.length)]
   driver.navigate.to a.attribute("href")
-  sleep(7)
+  sleep(5)
 end
